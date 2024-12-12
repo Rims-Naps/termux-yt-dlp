1fbaddb8368fee197a02edbf881fe3cc6f0a6db6
Snow-RSPS <108329399+Rims-Naps@users.noreply.github.com>
2024-12-12 12:01:41 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+
 # Clean Install
 rm -f "/data/data/com.termux/files/home/bin/termux-url-opener"
 mkdir -p /data/data/com.termux/files/home/bin
 
 # Installing dependencies
-pkg update -y && pkg install wget -y && pkg install python -y && pkg install ffmpeg -y
-yes | pip install yt-dlp
+pkg update -y && pkg upgrade -y
+pkg install -y wget python ffmpeg libexpat openssl
+pip install -U "yt-dlp[default]"
 
 # Downloading the modified termux-url-opener script
 wget --no-check-certificate --output-document="/data/data/com.termux/files/home/bin/termux-url-opener" "https://github.com/Rims-Naps/termux-yt-dlp/termux-url-opener"
